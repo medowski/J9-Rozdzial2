980213f076ab9644580bf8789a41dafb45e4dbb6
Tomasz Medowski <tomasz.medowski@gmail.com>
2020-03-31 23:01:18 +0200

    dodanie klasy Zadanie7 4. zmiana w master branchu

--- src/Zadanie7.java
+++ src/Zadanie7.java
@@ -1,4 +1,17 @@
+class Car {
+    int i = 0;
+    int odległość = 0;
+    int bak;
+
+    Car() {
+        bak=50;
+    }
+
+
+
+}
 public class Zadanie7 {
     Zadanie7() {
+        Car autko = new Car();
     }
 }
