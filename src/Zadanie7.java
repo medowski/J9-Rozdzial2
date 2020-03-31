d6c598ed26d6d593e8b9cc0c872b480bfe69760f
Tomasz Medowski <tomasz.medowski@gmail.com>
2020-03-31 22:30:34 +0200

    dodanie klasy Zadanie7 3. zmiana 2 Branchu

--- src/Zadanie7.java
+++ src/Zadanie7.java
@@ -1,5 +1,17 @@
+class Car {
+    int i = 10;
+    int j= 10;
+
+    Car() {
+        int i = 10;
+        int j = 10;
+    }
+}
+
 public class Zadanie7 {
     Zadanie7() {
+        System.out.println("To jest instrukcja z 2nd Branch");
+        Car autko = new Car();
         // DRUGA GAłĄŹ
     }
 }
