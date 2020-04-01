19b6ee9df00bd223fdd48ee4a6d80791e6895f26
Tomasz Medowski <tomasz.medowski@gmail.com>
2020-04-01 21:30:10 +0200

    dodanie klasy Zadanie7 5. zmiana w master branchu

---
+++ src/Zadanie8.java
@@ -0,0 +1,30 @@
+public class Zadanie8 {
+    int i,j,z;
+//    Integer i,j,z;
+    Zadanie8(int i) {
+        this.i = i;
+    }
+    Zadanie8(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+    Zadanie8(int i, int j, int z) {
+        this.i = i;
+        this.j = j;
+        this.z = z;
+    }
+    static void display(Zadanie8 z) {
+        System.out.println("Wynik: "+z.i+" "+z.j+" "+z.z);
+    }
+
+    public static void main(String[] args) {
+        Zadanie8 z1 = new Zadanie8(1);
+        Zadanie8 z2 = new Zadanie8(1,2);
+        Zadanie8 z3 = new Zadanie8(1,2,3);
+
+///        z1.display();
+        Zadanie8.display(z1);
+//        display(z2);
+    }
+
+}
---
+++ src/Zadanie9.java
@@ -0,0 +1,25 @@
+
+public class Zadanie9 {
+
+    class Car {
+        int droga = 0;
+        int benzyna = 50;
+        int spalanie;
+
+        Car() {
+            this.spalanie = 10;
+
+        }
+
+        void przejedźDrogę(int długośćDrogi) {
+
+        }
+        void zatankuj(int ilośćLitrów) {
+
+        }
+
+    }
+
+
+
+}
